85fc046e604382b9e6aed089df42c8e80e7760af
AnnaSyrtseva <77345297+AnnaSyrtseva@users.noreply.github.com>
2023-10-19 14:22:17 +0200

    Add files via upload

---
+++ Calc.java
@@ -0,0 +1,21 @@
+public class Calc {
+    public int sum(int a, int b) {
+        return a+b;
+    }
+
+    public int subtraction(int c, int d){
+        return d-c;
+    }
+
+    public double subDouble(double c, double d){
+        return d-c;
+    }
+
+    public int multi(int a, int b) {
+        return a*b;
+    }
+
+    public int division(int a, int b) {
+        return a/b;
+    }
+}
